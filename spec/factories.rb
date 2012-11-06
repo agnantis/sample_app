67b2b2849217962557f2fbb6b9f17ed07bc81210
Kostas Agnantis <kagnadis@gmail.com>
2012-11-06 15:42:06 +0200

    User signup finished

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Kwstas Agnantis"
+    email     "kwstas@example.com"
+    password  "test111"
+    password_confirmation "test111"
+  end
+end
